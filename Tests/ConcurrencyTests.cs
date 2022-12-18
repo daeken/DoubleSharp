65f3523787245cb1782c35ebc951834bbd56cc6f
Sera Brocious <sera.brocious@gmail.com>
2022-12-18 16:38:40 -0500

    Expanded concurrency tests to cover return from Increment/Decrement.

--- Tests/ConcurrencyTests.cs
+++ Tests/ConcurrencyTests.cs
@@ -22,29 +22,37 @@ public class ConcurrencyTests {
 		var count1 = 0;
 		Parallel.For(0, 10000, _ => count1.Increment());
 		Assert.That(count1, Is.EqualTo(10000));
+		Assert.That(count1.Increment(), Is.EqualTo(10001));
 		var count2 = 0U;
 		Parallel.For(0, 10000, _ => count2.Increment());
 		Assert.That(count2, Is.EqualTo(10000U));
+		Assert.That(count2.Increment(), Is.EqualTo(10001U));
 		var count3 = 0L;
 		Parallel.For(0, 10000, _ => count3.Increment());
 		Assert.That(count3, Is.EqualTo(10000L));
+		Assert.That(count3.Increment(), Is.EqualTo(10001L));
 		var count4 = 0UL;
 		Parallel.For(0, 10000, _ => count4.Increment());
 		Assert.That(count4, Is.EqualTo(10000UL));
+		Assert.That(count4.Increment(), Is.EqualTo(10001UL));
 	}
 	
 	[Test]
 	public void Decrement() {
-		var count1 = 10000;
+		var count1 = 10001;
+		Assert.That(count1.Decrement(), Is.EqualTo(10000));
 		Parallel.For(0, 10000, _ => count1.Decrement());
 		Assert.That(count1, Is.EqualTo(0));
-		var count2 = 10000U;
+		var count2 = 10001U;
+		Assert.That(count2.Decrement(), Is.EqualTo(10000U));
 		Parallel.For(0, 10000, _ => count2.Decrement());
 		Assert.That(count2, Is.EqualTo(0U));
-		var count3 = 10000L;
+		var count3 = 10001L;
+		Assert.That(count3.Decrement(), Is.EqualTo(10000L));
 		Parallel.For(0, 10000, _ => count3.Decrement());
 		Assert.That(count3, Is.EqualTo(0L));
-		var count4 = 10000UL;
+		var count4 = 10001UL;
+		Assert.That(count4.Decrement(), Is.EqualTo(10000UL));
 		Parallel.For(0, 10000, _ => count4.Decrement());
 		Assert.That(count4, Is.EqualTo(0UL));
 	}
