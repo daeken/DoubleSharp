9e54ba085a4a142d9ed8ed61445dcb171377570b
Sera Brocious <sera.brocious@gmail.com>
2022-12-20 10:37:43 -0500

    Added XML doc comments for BinaryIO. Made missing XML docs a build error.

--- DoubleSharp/IO/BinaryIOExtensions.cs
+++ DoubleSharp/IO/BinaryIOExtensions.cs
@@ -2,23 +2,56 @@ using System.Numerics;
 
 namespace DoubleSharp.IO; 
 
+/// <summary>
+/// DoubleSharp BinaryReader/BinaryWriter extensions.
+/// </summary>
 public static class BinaryIOExtensions {
+	/// <summary>
+	/// Read a Vector2 from this stream. The current position of the stream is advanced by eight.
+	/// </summary>
+	/// <param name="br">The BinaryReader to read from.</param>
+	/// <returns>A Vector2 read from the BinaryReader.</returns>
 	public static Vector2 ReadVector2(this BinaryReader br) =>
 		new(br.ReadSingle(), br.ReadSingle());
+	/// <summary>
+	/// Read a Vector3 from this stream. The current position of the stream is advanced by twelve.
+	/// </summary>
+	/// <param name="br">The BinaryReader to read from.</param>
+	/// <returns>A Vector3 read from the BinaryReader.</returns>
 	public static Vector3 ReadVector3(this BinaryReader br) => 
 		new(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
+	/// <summary>
+	/// Read a Vector4 from this stream. The current position of the stream is advanced by sixteen.
+	/// </summary>
+	/// <param name="br">The BinaryReader to read from.</param>
+	/// <returns>A Vector4 read from the BinaryReader.</returns>
 	public static Vector4 ReadVector4(this BinaryReader br) => 
 		new(br.ReadSingle(), br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
 
+	/// <summary>
+	/// Writes a Vector2 to this stream. The current position of the stream is advanced by eight.
+	/// </summary>
+	/// <param name="bw">The BinaryWriter to write to.</param>
+	/// <param name="vec">The Vector2 to write.</param>
 	public static void Write(this BinaryWriter bw, Vector2 vec) {
 		bw.Write(vec.X);
 		bw.Write(vec.Y);
 	}
+	/// <summary>
+	/// Writes a Vector3 to this stream. The current position of the stream is advanced by twelve.
+	/// </summary>
+	/// <param name="bw">The BinaryWriter to write to.</param>
+	/// <param name="vec">The Vector3 to write.</param>
 	public static void Write(this BinaryWriter bw, Vector3 vec) {
 		bw.Write(vec.X);
 		bw.Write(vec.Y);
 		bw.Write(vec.Z);
 	}
+	/// <summary>
+	/// Writes a Vector4 to this stream. The current position of the stream is advanced by sixteen.
+	/// </summary>
+	/// <param name="bw">The BinaryWriter to write to.</param>
+	/// <param name="vec">The Vector4 to write.</param>
 	public static void Write(this BinaryWriter bw, Vector4 vec) {
 		bw.Write(vec.X);
 		bw.Write(vec.Y);
